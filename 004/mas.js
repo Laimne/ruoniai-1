86b8108d2a937afa21460ebf07f50545892eddd5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-09 11:57:26 +0300

    go

--- 004/mas.js
+++ 004/mas.js
@@ -27,5 +27,21 @@ console.log(mas.length);
 //     console.log('Dabar Fancy:', aukstas, vienasGyventojas);
 // }
 
-
+//Esme
 mas.forEach((g, aukstas) => console.log(++aukstas, g));
+
+
+const mas2 = [];
+
+mas2.push([5, 8, 12]);
+mas2.push([45, 78, 112]);
+mas2.push([50, 80, 102]);
+// mas2.push(null);
+
+
+// console.table(mas2);
+
+// console.log(mas2[1][2]);
+
+
+mas2.forEach(aukstas => aukstas.forEach(skaicius => console.log(skaicius)));
