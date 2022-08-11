d8aa7decd7f83eb53557a47e4fdbe00616be3b08
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-11 11:04:03 +0300

    game

--- 006/dom.js
+++ 006/dom.js
@@ -1,5 +1,9 @@
-console.log('START');
-
+// console.log('START');
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 
 // setTimeout(() => {
@@ -11,14 +15,53 @@ console.log('START');
 // console.log('END');
 
 
-const keb = document.querySelector('button');
+// const keb = document.querySelector('button');
 
-console.log(keb);
+// console.log(keb);
 
-keb.addEventListener('click', () => {
-    console.log('Valio');
-    keb.style.display = 'none';
-    setTimeout(() => {
-        keb.style.display = 'block';
+// keb.addEventListener('click', () => {
+//     console.log('Valio');
+//     keb.style.display = 'none';
+//     setTimeout(() => {
+//         keb.style.display = 'block';
+//     }, 1000);
+// });
+
+// SPAGETI KODAS
+
+const c = document.querySelector('.circle');
+const h1 = document.querySelector('h1');
+let count = 0;
+let tid;
+let balls = 10;
+const start = () => {
+    const timerId = setInterval(() => {
+        c.style.top = rand(0, 90) + 'vh';
+        c.style.left = rand(0, 90) + 'vw';
+        balls--;
+        if (!balls) {
+            clearInterval(tid);
+            c.style.display = 'none';
+        }
+        h1.innerText = 'Good: ' + count + ' Left:' + balls;
     }, 1000);
-});
+    return timerId
+}
+h1.innerText = 'Good: ' + count + ' Left:' + balls;
+
+tid = start();
+
+c.addEventListener('click', () => {
+    clearInterval(tid);
+    tid = start();
+    c.style.top = rand(0, 90) + 'vh';
+    c.style.left = rand(0, 90) + 'vw';
+    balls--;
+    if (!balls) {
+        clearInterval(tid);
+        c.style.display = 'none';
+    }
+    count++;
+    h1.innerText = 'Good: ' + count + ' Left:' + balls;
+    console.log(count);
+})
