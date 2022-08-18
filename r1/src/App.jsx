eaf5a56d9a56b7bbb3a2512977d70cecbf9b5733
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-18 09:14:10 +0300

    komp

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Bebras from './Components/009/Bebras';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Bebras></Bebras>
         <h1>Labas</h1>
+        <Bebras></Bebras>
       </header>
     </div>
   );
---
+++ r1/src/Components/009/Bebras.jsx
@@ -0,0 +1,6 @@
+function Bebras() {
+
+    return <h2>Aš Bebras</h2>
+}
+
+export default Bebras;
