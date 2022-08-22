e9ca2b8f547104e8255c25e24ac3cd057eef23f9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-22 09:23:47 +0300

    kv

---
+++ r1/src/Components/010/Square.jsx
@@ -0,0 +1,10 @@
+function Square({color, name}) {
+
+    return (
+        <div className="cat App-logo" style={{ backgroundColor: color }}>
+            <span className="App-logo2">{name}</span>
+        </div>
+    )
+}
+
+export default Square;
