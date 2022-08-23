bd128a979e1105f17ea06f32859e464916c51485
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-23 10:08:25 +0300

    dogs

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -13,6 +13,16 @@ function App() {
                 dogs.map((d, i) => <div key={i} className={'dog ' + (i % 2 ? 'sq' : 'cc')} >{d}</div>)
             }
         </div>
+        <div className="dog-bin">
+            {
+                dogs.map((d, i) => (d[0] === d[0].toLocaleLowerCase()) ? <div key={i} className="dog sq">{d}</div> : null)
+            }
+        </div>
+        <div className="dog-bin">
+            {
+                dogs.map((d, i) => <div style={{color: d.length > 6 ? 'greenyellow' : 'crimson'}} key={i} className="dog sq">{d.length}</div>)
+            }
+        </div>
       </header>
     </div>
   );
