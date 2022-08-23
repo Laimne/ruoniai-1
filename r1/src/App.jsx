5ddaa59361b85bd404904e7170c6753ac366dcff
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-23 09:57:52 +0300

    suniai

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,13 +1,6 @@
-import './App.css';
-import Bin from './Components/010/Bin';
-import Square from './Components/010/Square';
+import './App.scss';
 
-const squares = [
-    {color: 'black', name:'Valio', size: 88, show: true},
-    {color: 'white', name:'Hello', size: 144, show: true},
-    {color: 'crimson', name:'Geras', size: 201, show: false},
-    {color: 'skyblue', name:'Neblogas', size: 333, show: true}
-];
+const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
 
 
 function App() {
@@ -15,10 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Props</h1>
-        <Bin color="crimson" size={188} />
+        <div className="dog-bin">
             {
-                squares.filter(sq => sq.size < 200).map((s, i) => <Square key={i} color={s.color} name={s.name} size={s.size} show={s.show} />)
+                dogs.map((d, i) => <div key={i} className={'dog ' + (i % 2 ? 'sq' : 'cc')} >{d}</div>)
             }
+        </div>
       </header>
     </div>
   );
