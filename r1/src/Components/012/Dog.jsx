ea6ae7b5f2702fb9647184c1672ce1fe37076f67
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-24 11:21:18 +0300

    show

--- r1/src/Components/012/Dog.jsx
+++ r1/src/Components/012/Dog.jsx
@@ -9,6 +9,8 @@ function Dog() {
     const [color2, setColor2] = useState(randColor());
     const [sq, setSq] = useState([]);
 
+    // let row = 0;
+
     const doIt = () => {
         // setSize(90);
         // setText('BIG');
@@ -22,7 +24,32 @@ function Dog() {
     };
 
     const add = () => {
-        setSq(s => [...s, {color: randColor(), number: rand(100, 999)}]);
+        setSq(s => [...s, {
+            color: randColor(),
+            number: rand(100, 999),
+            row: s.length,
+            show: true
+        }]);
+    }
+
+    const doSort = () => {
+        setSq(s => [...s].sort((a, b) => a.number - b.number));
+    }
+
+    const doSortReverse = () => {
+        setSq(s => [...s].sort((a, b) => b.number - a.number));
+    }
+
+    const doSortDefault = () => {
+        setSq(s => [...s].sort((a, b) => a.row - b.row));
+    }
+
+    const doFilterMore500 = () => {
+        setSq(s => s.map(sq => sq.number > 500 ? {...sq, show: true} : {...sq, show: false}))
+    }
+
+    const showAll = () => {
+        setSq(s => s.map(sq => ({...sq, show: true})))
     }
 
     return (
@@ -34,10 +61,15 @@ function Dog() {
         <button onClick={doIt}>Press this button, please</button>
         <div className="dog-bin">
             {
-                sq.map((c, i) => <div style={{backgroundColor: c.color}} className="dog sq" key={i}>{c.number}</div>)
+                sq.map((c, i) => c.show ? <div style={{backgroundColor: c.color}} className="dog sq" key={i}>{c.number}</div> : null)
             }
         </div>
         <button onClick={add}>[]</button>
+        <button onClick={doSort}>sort AZ</button>
+        <button onClick={doSortReverse}>sort ZA</button>
+        <button onClick={doSortDefault}>sort Default</button>
+        <button onClick={doFilterMore500}>show more 500</button>
+        <button onClick={showAll}>show all</button>
         </>
     );
 
