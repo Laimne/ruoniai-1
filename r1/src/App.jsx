3a50f3a384b29b7ebf3c60d9cbad5ef2090339f7
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-25 11:31:16 +0300

    go go

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,12 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.scss';
-import Dog from './Components/012/Dog';
+import Books from './Components/013/Books';
+import axios from 'axios';
 
 function App() {
+
+    const [books, setBooks] = useState(null);
+
+    // fetch('https://in3.dev/knygos/')
+    // .then((response) => response.json())
+    // .then((data) => setBooks(data))
+    // .catch(_ => setBooks('ERROR'));
+
+    useEffect(() => {
+        axios.get('https://in3.dev/knygos/')
+        .then(res => setBooks(res.data))
+        .catch(_ => setBooks('ERROR'))
+    }, []);
+   
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>STATE</h1>
-        <Dog color="crimson" />
+        <Books books={books} />
       </header>
     </div>
   );
